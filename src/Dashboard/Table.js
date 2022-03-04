32654cad287a5460835218210514ee55c482b34e
Laisvunas <saugos@gmail.com>
2022-03-04 17:39:28 +0200

    Add table dark theme

--- src/Dashboard/Table.js
+++ src/Dashboard/Table.js
@@ -5,20 +5,23 @@ import { AutoSizer, Column, Table } from 'react-virtualized';
 import 'react-virtualized/styles.css';
 import { Card } from './Card';
 import { medGrey } from './GlobalStyle';
+import { ThemeContext } from './Dashboard';
+import { brandColor } from './GlobalStyle';
 
 const ROW_HEIGHT = 48;
 
 const TableCell = styled.div`
   display: flex;
-  color: black;
+  color: ${({ header, dark }) => dark ? (header ? brandColor : 'white') : 'black'};
   ${({ align }) => align === 'right' && `flex-direction: row-reverse`}
 `;
 
 class ReactVirtualizedTable extends React.Component {
   cellRenderer = ({ cellData, columnIndex }) => {
-    const { columns } = this.props;
+    const { columns, dark } = this.props;
     return (
       <TableCell
+      dark={dark}
         align={columns[columnIndex].numeric ? 'right' : 'left'}
       >
         {cellData}
@@ -26,9 +29,10 @@ class ReactVirtualizedTable extends React.Component {
     );
   };
   headerRenderer = ({ label, columnIndex }) => {
-    const { columns } = this.props;
+    const { columns, dark } = this.props;
     return (
       <TableCell
+        dark={dark}  
         header
         align={columns[columnIndex].numeric ? 'right' : 'left'}
       >
@@ -37,7 +41,7 @@ class ReactVirtualizedTable extends React.Component {
     );
   };
   render() {
-    const { columns } = this.props;
+    const { columns, dark } = this.props;
     return (
       <AutoSizer>
         {({ height, width }) => (
@@ -49,7 +53,7 @@ class ReactVirtualizedTable extends React.Component {
             rowCount={tableRows.length}
             rowGetter={({ index }) => tableRows[index]}
             rowStyle={{
-              borderBottom: `1px solid ${medGrey}`,
+              borderBottom: `1px solid ${dark ? 'grey' : medGrey}`,
               boxSizing: 'border-box',
             }}
             gridStyle={{ outline: 0 }}
@@ -76,9 +80,11 @@ class ReactVirtualizedTable extends React.Component {
 }
 
 export function VirtualizedTable() {
+  const [theme, setTheme] = useContext(ThemeContext);
+  const dark = theme == 'dark';
   return (
-    <Card height={400}>
-      <ReactVirtualizedTable columns={tableColumns} />
+    <Card height={400} dark={dark}>
+      <ReactVirtualizedTable dark={dark} columns={tableColumns} />
     </Card>
   );
 }
